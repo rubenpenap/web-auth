f3b3ff67f04edd69053b63812ef4d390fd87f24d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-13 15:16:43 -0400

    fix: streamline theme management by refining getTheme and setTheme functions

--- exercises/01.cookies/02.problem.theme/app/utils/theme.server.ts
+++ exercises/01.cookies/02.problem.theme/app/utils/theme.server.ts
@@ -1,22 +1,15 @@
-// 🐨 you're going to need the cookie module here:
-// 💰 import * as cookie from 'cookie'
+import * as cookie from 'cookie'
 
+const cookieName = 'theme'
 export type Theme = 'light' | 'dark'
 
-// 🦉 The default for us will be 'light', so if we can't determine the theme,
-// then you'll want to return 'light'
-
 export function getTheme(request: Request): Theme {
-	// 🐨 get the cookie header from the request
-	// 🐨 if the cookie header is `null`, return the default
-	// 🐨 parse the cookie header
-	// 🐨 get the value for the cookie called 'theme'
-	// 🐨 if the value is a valid cookie value, return it, otherwise return the default
+	const cookieHeader = request.headers.get('cookie')
+	const parsed = cookieHeader ? cookie.parse(cookieHeader)[cookieName] : 'light'
+	if (parsed === 'light' || parsed === 'dark') return parsed
 	return 'light'
 }
 
 export function setTheme(theme: Theme) {
-	// 🐨 serialize the theme as a cookie called 'theme'
-	// 💰 make sure to set the path to '/' so it applies to the entire site.
-	return ''
+	return cookie.serialize(cookieName, theme, { path: '/' })
 }
