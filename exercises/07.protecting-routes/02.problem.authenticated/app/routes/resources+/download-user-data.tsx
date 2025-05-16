0698998e06196adf99ee4450c8fe3dedbc0dcb36
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-16 17:04:52 -0400

    refactor: update loader in download user data route to use requireUserId for user authentication

--- exercises/07.protecting-routes/02.problem.authenticated/app/routes/resources+/download-user-data.tsx
+++ exercises/07.protecting-routes/02.problem.authenticated/app/routes/resources+/download-user-data.tsx
@@ -1,9 +1,10 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { getDomainUrl } from '#app/utils/misc.tsx'
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const userId = 'some_user_id' // 🐨 get the user with your requireUserId util
+	const userId = await requireUserId(request)
 	const user = await prisma.user.findUniqueOrThrow({
 		where: { id: userId },
 		// this is one of the *few* instances where you can use "include" because
