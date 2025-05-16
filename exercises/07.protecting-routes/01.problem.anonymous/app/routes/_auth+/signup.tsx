2a682c6f594df36048427d65b64c560628d9c461
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-16 16:30:38 -0400

    refactor: implement loader in signup route to enforce anonymous access using requireAnonymous utility

--- exercises/07.protecting-routes/01.problem.anonymous/app/routes/_auth+/signup.tsx
+++ exercises/07.protecting-routes/01.problem.anonymous/app/routes/_auth+/signup.tsx
@@ -4,6 +4,7 @@ import {
 	json,
 	redirect,
 	type ActionFunctionArgs,
+	type LoaderFunctionArgs,
 	type MetaFunction,
 } from '@remix-run/node'
 import { Form, useActionData } from '@remix-run/react'
@@ -15,6 +16,7 @@ import { Spacer } from '#app/components/spacer.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
 import {
 	getSessionExpirationDate,
+	requireAnonymous,
 	signup,
 	userIdKey,
 } from '#app/utils/auth.server.ts'
@@ -53,11 +55,13 @@ const SignupFormSchema = z
 		}
 	})
 
-// 🐨 create a loader here that uses the requireAnonymous utility and returns
-// an empty object of json.
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requireAnonymous(request)
+	return json({})
+}
 
 export async function action({ request }: ActionFunctionArgs) {
-	// 🐨 add the requireAnonymous utility here
+	await requireAnonymous(request)
 	const formData = await request.formData()
 	await validateCSRF(formData, request.headers)
 	checkHoneypot(formData)
