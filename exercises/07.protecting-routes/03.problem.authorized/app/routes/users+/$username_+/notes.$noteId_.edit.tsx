b76dc5e7a183c67d25075b557bb798784b1caa4a
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-16 23:13:41 -0400

    refactor: enhance user authentication in note editing route by implementing requireUser and invariantResponse

--- exercises/07.protecting-routes/03.problem.authorized/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ exercises/07.protecting-routes/03.problem.authorized/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,15 +1,17 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import { requireUser } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { invariantResponse } from '#app/utils/misc.tsx'
 import { NoteEditor, action } from './__note-editor.tsx'
 
 export { action }
 
-export async function loader({ params }: LoaderFunctionArgs) {
-	// 🐨 require the user and check that the user.username is equal to params.username.
-	// If not, then throw a 403 response
-	// 💰 you can use invariantResponse for this.
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	const user = await requireUser(request)
+	invariantResponse(user.username === params.username, 'Forbidden', {
+		status: 403,
+	})
 	const note = await prisma.note.findFirst({
 		select: {
 			id: true,
@@ -24,9 +26,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
 		},
 		where: {
 			id: params.noteId,
-			// 🐨 you can switch this to: "ownerId: user.id" which should make the
-			// query more efficient.
-			owner: { username: params.username },
+			ownerId: user.id,
 		},
 	})
 	invariantResponse(note, 'Not found', { status: 404 })
