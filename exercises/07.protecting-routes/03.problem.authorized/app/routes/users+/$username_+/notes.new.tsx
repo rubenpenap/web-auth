0390acad3fdc77fafdd5ed84d2dfbde7f7d2696c
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-16 23:13:50 -0400

    refactor: implement user authentication in notes creation route by adding loader with requireUser and invariantResponse

--- exercises/07.protecting-routes/03.problem.authorized/app/routes/users+/$username_+/notes.new.tsx
+++ exercises/07.protecting-routes/03.problem.authorized/app/routes/users+/$username_+/notes.new.tsx
@@ -1,9 +1,15 @@
+import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import { requireUser } from '#app/utils/auth.server.ts'
+import { invariantResponse } from '#app/utils/misc.tsx'
 import { action, NoteEditor } from './__note-editor.tsx'
 
-// 🐨 add a loader here that requires the user and checks that the user.username
-// is equal to params.username. If not, then throw a 403 response
-// 💰 you can use invariantResponse for this.
-// 🐨 Return json({}) because Remix requires loaders return something.
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	const user = await requireUser(request)
+	invariantResponse(user.username === params.username, 'Forbidden', {
+		status: 403,
+	})
+	return json({})
+}
 
 export { action }
 export default NoteEditor
