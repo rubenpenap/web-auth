90a05e0b9e7c80f2f72edb13d357dfa7e1b23c14
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-19 14:32:08 -0400

    fix: connect new user to the "user" role in signup function

--- exercises/08.permissions/02.problem.seed/app/utils/auth.server.ts
+++ exercises/08.permissions/02.problem.seed/app/utils/auth.server.ts
@@ -96,7 +96,7 @@ export async function signup({
 			email: email.toLowerCase(),
 			username: username.toLowerCase(),
 			name,
-			// 🐨 connect this new user with the role named "user"
+			roles: { connect: { name: 'user' } },
 			password: {
 				create: {
 					hash: hashedPassword,
