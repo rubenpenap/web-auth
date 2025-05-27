b78116bd2892f57adc96b023b7d76eb691a3a338
Ruben Peña <46791460+rubenpenap@users.noreply.github.com>
2025-05-27 18:02:17 -0400

    feat: implement GitHub authentication action function to handle user login

--- exercises/17.oauth/02.problem.flow/app/routes/_auth+/auth.github.ts
+++ exercises/17.oauth/02.problem.flow/app/routes/_auth+/auth.github.ts
@@ -1,13 +1,10 @@
-import { redirect } from '@remix-run/node'
-// 💰 you're gonna need this:
-// import { authenticator } from '#app/utils/auth.server.ts'
+import { type ActionFunctionArgs, redirect } from '@remix-run/node'
+import { authenticator } from '#app/utils/auth.server.ts'
 
 export async function loader() {
 	return redirect('/login')
 }
 
-export async function action() {
-	// 🐨 call authenticator.authenticate with 'github', the request. Return the
-	// result.
-	return new Response('not implemented', { status: 500 })
+export async function action({request}: ActionFunctionArgs) {
+	return await authenticator.authenticate('github', request)
 }
