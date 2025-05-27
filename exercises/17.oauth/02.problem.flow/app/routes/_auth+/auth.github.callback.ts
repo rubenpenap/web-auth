95505df42eecbf624f0263fad6f71c7a0cf8a161
Ruben Peña <46791460+rubenpenap@users.noreply.github.com>
2025-05-27 18:02:10 -0400

    feat: implement GitHub authentication loader function with error handling and logging

--- exercises/17.oauth/02.problem.flow/app/routes/_auth+/auth.github.callback.ts
+++ exercises/17.oauth/02.problem.flow/app/routes/_auth+/auth.github.callback.ts
@@ -1,13 +1,10 @@
-// 💰 you're gonna need this:
-// import { authenticator } from '#app/utils/auth.server.ts'
+import { type LoaderFunctionArgs } from '@remix-run/node'
+import { authenticator } from '#app/utils/auth.server.ts'
 import { redirectWithToast } from '#app/utils/toast.server.ts'
 
-export async function loader() {
-	// 🐨 call authenticator.authenticate with 'github', the request, and
-	// the option throwOnError: true
-
-	// 🐨 feel free to log the result
-
+export async function loader({request}: LoaderFunctionArgs) {
+	const data = await authenticator.authenticate('github', request, {throwOnError: true})
+	console.log(data)
 	throw await redirectWithToast('/login', {
 		title: 'Auth Success (jk)',
 		description: `You have successfully authenticated with GitHub (not really though...).`,
