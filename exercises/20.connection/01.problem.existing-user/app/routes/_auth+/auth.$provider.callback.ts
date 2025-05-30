2afaa9e999ac39cb0e578afae7ec89288c8f9b02
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-29 22:01:01 -0400

    feat: enhance authentication callback to create user connections and display success toast message

--- exercises/20.connection/01.problem.existing-user/app/routes/_auth+/auth.$provider.callback.ts
+++ exercises/20.connection/01.problem.existing-user/app/routes/_auth+/auth.$provider.callback.ts
@@ -6,7 +6,10 @@ import {
 } from '#app/utils/auth.server.ts'
 import { ProviderNameSchema, providerLabels } from '#app/utils/connections.tsx'
 import { prisma } from '#app/utils/db.server.ts'
-import { redirectWithToast } from '#app/utils/toast.server.ts'
+import {
+	createToastHeaders,
+	redirectWithToast,
+} from '#app/utils/toast.server.ts'
 import { verifySessionStorage } from '#app/utils/verification.server.ts'
 import { handleNewSession } from './login.tsx'
 import {
@@ -55,14 +58,30 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 		return makeSession({ request, userId: existingConnection.userId })
 	}
 
-	// 🐨 find a user by the profile.email and if a user exists, then create a
-	// new connection for that user and return a call to makeSession
-	// 💯 redirect them to '/settings/profile/connections'
-	// 💯 use `createToastHeaders` to add a header to create a toast message:
-	// {
-	// 	title: 'Connected',
-	// 	description: `Your "${profile.username}" ${label} account has been connected.`,
-	// }
+	const user = await prisma.user.findUnique({
+		select: { id: true },
+		where: { email: profile.email },
+	})
+
+	if (user) {
+		await prisma.connection.create({
+			data: { providerName, providerId: profile.id, userId: user.id },
+		})
+		return makeSession(
+			{
+				request,
+				userId: user.id,
+				redirectTo: '/settings/profile/connections',
+			},
+			{
+				headers: await createToastHeaders({
+					type: 'success',
+					title: 'Connected',
+					description: `Your "${profile.username}" ${label} account has been connected.`,
+				}),
+			},
+		)
+	}
 
 	// this is a new user, so let's get them onboarded
 	const verifySession = await verifySessionStorage.getSession(
